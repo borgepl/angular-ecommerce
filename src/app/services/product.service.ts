3964d40b7acee42508ce4788553b3dcb9f346c4b
borgepl <pdeborger@gmail.com>
2021-12-14 15:05:22 +0100

    update search product-catergories

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,10 +10,17 @@ export class ProductService {
 
   private baseUrl = "http://localhost:8082/api/products";
 
+  //set size to 100 items - just for testing - Data Rest by default only retrieves 20 items
+  //private baseUrl = "http://localhost:8082/api/products?size=100";
+
+
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetRespone>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetRespone>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
