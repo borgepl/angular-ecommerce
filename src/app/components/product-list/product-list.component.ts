d983afe51ccd458b8e75ecb0849de163b7fb78b7
borgepl <pdeborger@gmail.com>
2022-01-05 11:26:19 +0100

    pagination - selecting page size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -19,7 +19,7 @@ export class ProductListComponent implements OnInit {
 
   // properties for pagination
   thePageNumber: number =  1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
   
 
@@ -86,5 +86,10 @@ export class ProductListComponent implements OnInit {
     }
   }
 
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  } 
 
 }
