a256fb8dd0a5d36d89538f1945f7d64042cd773f
borgepl <pdeborger@gmail.com>
2022-01-19 16:46:16 +0100

    Self registration for users

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -26,6 +26,9 @@ export class LoginComponent implements OnInit {
        * we derive it from the given issuer for convenience.
        */
        logo: 'assets/images/logo.png',
+       features: {
+         registration: true
+       },
        baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
        clientId: myAppConfig.oidc.clientId,
        redirectUri: myAppConfig.oidc.redirectUri,
