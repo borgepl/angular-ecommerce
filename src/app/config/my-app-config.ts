2263cedf11761e9cd815c65acb56e6f8dcbdcb35
borgepl <pdeborger@gmail.com>
2022-01-25 13:55:42 +0100

    Secure HTTPS self-signed cert

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -5,7 +5,7 @@ export default {
         //issuer: 'https://<<UPDATE-WITH-YOUR-DEV-DOMAIN>>/oauth2/default',
         clientId: '0oa3lf77axrYkDdvs5d7',
         issuer: 'https://dev-38652625.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     },
     widget: {
