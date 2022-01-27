b2083493e07716061d9cf616543b3fe574e03333
borgepl <pdeborger@gmail.com>
2022-01-27 15:04:40 +0100

    Bug fix for total amount

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -225,8 +225,10 @@ export class CheckoutComponent implements OnInit {
      purchase.orderItems = orderItems;
  
      // compute payment info
-    this.paymentInfo.amount = this.totalPrice * 100;
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
+
+    console.log(`paymentInfo.amount : ${this.paymentInfo.amount}`);
 
     // if valid form then
     // - create payment intent
